4692dde8ab2f26a25e23ccea8a5fccc8299b38d7
Julien Delhez <j.delhez@student.helmo.be>
2026-04-10 22:26:11 +0200

    Refactor MENU structure to use module paths and function names for improved clarity and dynamic imports

--- Chap_5.py
+++ Chap_5.py
@@ -8,40 +8,41 @@ from Func_Chap5.Int_M_N     import Int_M_N
 from Func_Chap5.Int_M_M_N   import Int_M_M_N
 
 MENU = [
-    (1, "Tract",    N_pl_Rd),
-    (2, "Compr",    N_c_Rd),
-    (3, "Flexion",  M_Rd),
-    (4, "Shear",    V_pl_Rd),
-    (5, "M-V",      Int_M_V),
-    (6, "M-M",      Int_M_M),
-    (7, "M-N",      Int_M_N),
-    (8, "M-M-N",    Int_M_M_N),
+    (1, "Tract",    "Func_Chap5.Traction", "N_pl_Rd"),
+    (2, "Compr",    "Func_Chap5.Compression", "N_c_Rd"),
+    (3, "Flexion",  "Func_Chap5.Flexion", "M_Rd"),
+    (4, "Shear",    "Func_Chap5.V_pl_Rd", "V_pl_Rd"),
+    (5, "M-V",      "Func_Chap5.Int_M_V", "Int_M_V"),
+    (6, "M-M",      "Func_Chap5.Int_M_M", "Int_M_M"),
+    (7, "M-N",      "Func_Chap5.Int_M_N", "Int_M_N"),
+    (8, "M-M-N",    "Func_Chap5.Int_M_M_N", "Int_M_M_N"),
 ]
 
 print("Choose a function:")
 while True:
     for i in range(0, len(MENU), 2):
-        k1, label1, _ = MENU[i]
+        k1, label1, module_path, func_name = MENU[i]
         line = "{}: {}".format(k1, label1)
         if i + 1 < len(MENU):
-            k2, label2, _ = MENU[i + 1]
+            k2, label2, module_path, func_name = MENU[i + 1]
             line += "  {}: {}".format(k2, label2)
         print(line)
 
     try:
-        choice = int(input("Choice (1-9): "))
+        choice = int(input("Choice (1-8): "))
     except ValueError:
-        print("Enter 1-9")
+        print("Enter 1-8")
         continue
 
     fn = None
-    for k, _, f in MENU:
+    for k, label, module_path, func_name in MENU:
         if k == choice:
-            fn = f
+            module = __import__(module_path, fromlist=[func_name])
+            fn = getattr(module, func_name)
+            fn()
             break
     
     if fn:
-        fn()
         break
     else:
-        print("Enter 1-9")
+        print("Enter 1-8")
